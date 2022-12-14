2befdac17832782b385d3fce288838976554393c
VladanStar <vladancupric@gmail.com>
2022-12-14 12:10:48 +0100

    Add

--- Exercises01/task02/main.js
+++ Exercises01/task02/main.js
@@ -1,3 +1,7 @@
 function addElement (){
-    var newDiv = document.createElement("div")
+    var newDiv = document.createElement("div");
+    var h1 = document.createElement("H1");
+    var text  = document.createTextNode("Hello World");
+    h1.appendChild(text);
+    document.body.appendChild(h1)
 }
